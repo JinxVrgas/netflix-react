d6a965b9a9ddabbe9586a8e55b962341c3a4a214
Jinson <jinsonvarghese9048@gmail.com>
2024-11-25 07:02:30 +0530

    fetch vedio link from tmdb and on clicking a movie card you tube video player will paly trailer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import HomePage from './Pages/Home/HomePage'
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './Pages/Login/LoginPage'
+import PlayerPage from './Pages/Player/PlayerPage'
 
 const App = () => {
   return (
     <div>
       <Routes>
-      <Route path='/' element={<HomePage />}/>
-      <Route path='/login' element={<LoginPage />}/>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/player/:id' element={<PlayerPage />} />     
 
 
       </Routes>
--- src/Pages/Player/PlayerPage.jsx
+++ src/Pages/Player/PlayerPage.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './player.css'
+import back_arrow from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 const PlayerPage = () => {
+
+
+  const { id } = useParams();
+
+
+  const [apidata, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  });
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDRlZjJjNjFhMWRjYWRlYjZlZDQyMzVmMDE4OGQ2ZCIsIm5iZiI6MTczMjUxMzkyMS4zMjA3ODQ4LCJzdWIiOiI2NzQ0MGVmNWNjMWQ2OTk4NmJkOWNjYWUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.osIpjt3bxkig12YdNqIzz3Sxc30530NmStPr60si6sw'
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results[0]))
+      .catch(err => console.error(err));
+
+  }, [])
+
+
   return (
-    <div className='player'>PlayerPage</div>
+    <div className='player'>
+      <img src={back_arrow} alt="" />
+      <iframe
+        width="90%"
+        height='90%'
+        src={`https://www.youtube.com/embed/${apidata.key}`}
+        title='trainer'
+        frameBorder="0"
+        allowFullScreen></iframe>
+      <div className="player_info">
+        <p>{apidata.published_at.slice(0, 10)}</p>
+        <p>{apidata.name}</p>
+        <p>{apidata.type}</p>
+      </div>
+    </div>
   )
 }
 
